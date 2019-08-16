59e4071388ce499925fb7195b6a63523c0e934d5
Jacqueline Remmel <jpremmel@gmail.com>
2019-08-16 09:42:56 -0700

    Add JS if condition to make sure user answers all of the questions.

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,15 @@ $(document).ready(function() {
   $("#quiz").submit(function(event) {
     event.preventDefault();
     $(".results").hide();
-
     var q1=$("input:radio[name=q1]:checked").val();
     var q2=$("input:radio[name=q2]:checked").val();
     var q3=$("input:radio[name=q3]:checked").val();
     var q4=$("input:radio[name=q4]:checked").val();
     var q5=$("input:radio[name=q5]:checked").val();
 
-    
+    if (!q1 || !q2 || !q3 || !q4 || !q5) {
+      alert("Please answer all of the questions.");
+    }
+
   });
 });
