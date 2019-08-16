4a428e8093354712fcf5c5ade271c152726e28c4
Jacqueline Remmel <jpremmel@gmail.com>
2019-08-16 16:22:20 -0700

    Update all language results to include user's name.

--- js/scripts.js
+++ js/scripts.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
     } else {
       $("#python").show();
     }
-    $("#output").text(name);
+    $(".output").text(name);
   });
   $("#clear").click(function() {
     $("#quiz").trigger("reset");
