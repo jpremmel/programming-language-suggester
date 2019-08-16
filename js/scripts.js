cfafcaef8f3e2c528115f170639a6413b6f436da
Jacqueline Remmel <jpremmel@gmail.com>
2019-08-16 09:39:47 -0700

    Add JS to store values of quiz responses into variables when the quiz is submitted.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-  
+  $("#quiz").submit(function(event) {
+    event.preventDefault();
+    $(".results").hide();
+
+    var q1=$("input:radio[name=q1]:checked").val();
+    var q2=$("input:radio[name=q2]:checked").val();
+    var q3=$("input:radio[name=q3]:checked").val();
+    var q4=$("input:radio[name=q4]:checked").val();
+    var q5=$("input:radio[name=q5]:checked").val();
+
+    
+  });
 });
