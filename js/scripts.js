d884a2efe37a880bb744133716f2314cbbca918d
Jacqueline Remmel <jpremmel@gmail.com>
2019-08-16 16:04:58 -0700

    Update if condition to make user enter a name and add user's name into html for JavaScript result.

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,12 @@ $(document).ready(function() {
     event.preventDefault();
     $(".results").hide();
     var name=$("input#name").val();
-    console.log(name);
     var q1=$("input:radio[name=q1]:checked").val();
     var q2=$("input:radio[name=q2]:checked").val();
     var q3=$("input:radio[name=q3]:checked").val();
     var q4=$("input:radio[name=q4]:checked").val();
     var q5=$("input:radio[name=q5]:checked").val();
-
-    if (!q1 || !q2 || !q3 || !q4 || !q5) {
+    if (!q1 || !q2 || !q3 || !q4 || !q5 || !name) {
       alert("Please answer all of the questions.");
     } else if (q1 === "yes" && q3 != "yes" && q5 != "yes") {
       $("#js").show();
@@ -25,6 +23,7 @@ $(document).ready(function() {
     } else {
       $("#python").show();
     }
+    $("#output").text(name);
   });
   $("#clear").click(function() {
     $("#quiz").trigger("reset");
